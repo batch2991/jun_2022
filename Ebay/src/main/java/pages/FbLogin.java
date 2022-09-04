f01da18dc7e17c8aab5cb3bca4670fcfd373fee2
venkat <venkatgn.gutta@gmail.com>
2022-09-04 19:12:21 +0530

    fblogin and classes.html are added

---
+++ Ebay/src/main/java/pages/FbLogin.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FbLogin 
+{
+   
+   ChromeDriver driver;
+	
+   public void openUrl()
+   {
+	   WebDriverManager.chromedriver().setup();
+       driver=new ChromeDriver();
+       driver.get("http://www.facebook.com");
+   }
+   public void login(String uid,String pwd)
+   {
+	   driver.findElement(By.name("email")).sendKeys(uid);
+       driver.findElement(By.name("pass")).sendKeys(pwd);
+       driver.findElement(By.name("login")).click();
+	   
+   }
+   public void validateErrormsg()
+   {
+	   System.out.println("code to read error message and validate");     
+   }
+   public void validatesuccesslogin()
+   {
+	   System.out.println("check the home is displayed");
+   }
+}
