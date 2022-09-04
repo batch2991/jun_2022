7e4a0c96ecaf689144dfc880d71b0b2dbb40d28c
venkat <venkatgn.gutta@gmail.com>
2022-09-04 20:00:06 +0530

    order.java updated createorder,updateorder methods, invoice.java class created

--- Ebay/src/main/java/pages/orders.java
+++ Ebay/src/main/java/pages/orders.java
@@ -4,10 +4,10 @@ public class orders
 {
   public void createoder()
   {
-	  
+	  System.out.println("code to creat new order");
   }
   public void updateorder()
   {
-	  
+	  System.out.println("code to update order");
   }
 }
